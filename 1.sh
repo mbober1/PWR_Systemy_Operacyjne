d7c066db0716a5d848a1f9c2a9fe6a244ba13ede
Marcin Bober <mbober1@gmail.com>
2020-10-14 20:02:10 +0200

    koniec

--- 1.sh
+++ 1.sh
@@ -2,13 +2,9 @@
 
 VAR1=`date | cut -d ',' -f 1`
 
-echo $VAR1
-
-if [ $VAR1=="sob" ]
+if [ "$VAR1" = "sob" ] || [ "$VAR1" = "nie" ]
 then
   echo "Jest weekend! Pijemy!"
-elif [ $VAR1=="ndz" ]
-  echo "Jest weekend! Ale dzisiaj nie pijemy :/"
 else
   echo "Jest zwykły dzień :/"
 fi

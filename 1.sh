96fa2bf308f821dd61799033480854f97804d79f
Marcin Bober <mbober1@gmail.com>
2020-10-14 19:30:07 +0200

    jezu działa w końcu

--- 1.sh
+++ 1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# a = date | cut -d ',' -f 1
-STR= date | cut -d ',' -f 1 | grep "śro"
+
+VAR1= date | cut -d ',' -f 1
 
 echo $VAR1
-if [ "$VAR1" = "śro" ]; then
+
+if [ $VAR1=="śro" ]; then
     echo "Jest zwykły dzień :/"
 else
     echo "Jest weekend! Pijemy!"
